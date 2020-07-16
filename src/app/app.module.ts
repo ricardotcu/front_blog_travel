a48fc9d721066f20a3cd671b5abe58423fd0e4cc
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 13:00:45 -0300

    ajuste1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { FavoritosComponent } from './favoritos/favoritos.component';
+import { PostsComponent } from './posts/posts.component';
+import { ContatoComponent } from './contato/contato.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    FavoritosComponent,
+    PostsComponent,
+    ContatoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
