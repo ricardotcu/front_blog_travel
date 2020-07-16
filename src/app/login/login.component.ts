b04989331dd08186e02eb92edf4d19f62d55d407
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 15:37:56 -0300

    ajuste10

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  profileForm = new FormGroup({
+    email: new FormControl(''),
+    senha: new FormControl(''),
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+  }
+  
+  onSubmit() {
+    this.login();
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  profileForm = new FormGroup({
+    nome: new FormControl(''),
+    email: new FormControl(''),
+    senha: new FormControl(''),
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  register(){
+  }
+  
+  onSubmit() {
+    this.register();
   }
 
 }
