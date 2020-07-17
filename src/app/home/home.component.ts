538945652805316764f097ec76f3be3540cc795c
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-16 23:41:28 -0300

    ajuste45

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = 'home';	
+  readonly apiURL : string;
+  public rota: Router;
+  public resumo: any;
 
-  constructor() { }
+  constructor(private http : HttpClient, private r: Router){
+    this.apiURL = 'https://back-end-travel.herokuapp.com';
+    this.rota = r;
+  }
+
 
   ngOnInit(): void {
+    this.http.get(`${this.apiURL}/`)
+      .subscribe(result => {
+        this.resumo = result;
+        console.log(this.resumo)
+      });
   }
 
 }
