d09be73bc93eff15ecc0a8998e88970e7273e078
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-17 02:20:19 -0300

    ajuste52

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +15,31 @@ export class RegisterComponent implements OnInit {
     email: new FormControl(''),
     senha: new FormControl(''),
   });
+  title = 'register';	
+  readonly apiURL : string;
+  public rota: Router;
 
-  constructor() { }
+  constructor(private http : HttpClient, private r: Router){
+    this.apiURL = 'https://back-end-travel.herokuapp.com';
+    this.rota = r;
+  }
 
   ngOnInit(): void {
   }
-
-  register(){
+  
+  register(user: User) {
+    console.log(user)
+    this.http.post(`${this.apiURL}/register`, user)
+      .subscribe(result => {
+        console.log(result);
+        window.localStorage.setItem('currentUser', JSON.stringify(result));
+        console.log(window.localStorage.getItem('currentUser'));
+        this.r.navigate(['/home']);
+      });
   }
   
   onSubmit() {
-    this.register();
+    this.register(this.profileForm.value);
   }
 
 }
